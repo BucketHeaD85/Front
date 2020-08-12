e57d6c76ca05044718b8dc48897610a4373779a0
Daniel Toro <daniel.torov@sansano.usm.cl>
2020-08-12 18:59:07 -0400

    Https

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const endpoints = {
-    development: 'http://isw-ayudantia.herokuapp.com',
+    development: 'https://isw-ayudantia.herokuapp.com',
     //development: 'http://127.0.0.1:8000',
 };
 
